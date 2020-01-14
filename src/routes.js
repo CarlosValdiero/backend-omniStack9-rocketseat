9bf9a8aebcc20a8336d2961e18270c6224fc118c
Carlos Valdiero <carlos_valdiero@hotmail.com>
2020-01-14 12:11:42 -0300

    notifications implemented with socketio

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ const SessionController = require("./Controllers/SessionController");
 const SpotController = require("./Controllers/SpotController");
 const DashboardController = require("./Controllers/DashboardController");
 const BookingController = require("./Controllers/BookingController");
+const ApprovalController = require("./Controllers/ApprovalController");
+const RejectionController = require("./Controllers/RejectController");
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -18,5 +20,8 @@ routes.get('/spots', SpotController.index);
 routes.get('/dashboards', DashboardController.show);
 
 routes.post('/spots/:spot_id/bookings', BookingController.store);
+
+routes.post('/bookings/:booking_id/approvals',ApprovalController.store);
+routes.post('/bookings/:booking_id/rejections',RejectionController.store);
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const mongoose = require("mongoose");
 const cors = require('cors');
-const routes = require('./routes');
 const path = require('path');
+
+const socketio = require('socket.io');
+const http = require('http');
+
+const routes = require('./routes');
+
 require('dotenv').config();
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect(process.env.DATABASE_CONNECTION_STRING,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
+});
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    console.log('conected user',socket.id);
+
+    const {user_id} = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
 });
 
 // GET, POST, PUT, DELETE
@@ -18,6 +35,13 @@ mongoose.connect(process.env.DATABASE_CONNECTION_STRING,{
 //req.params == acessar route params (para edicao e delite)
 //req.body == acessar corpo da requisicao (para criacao e edicao)
 
+app.use((req,res,next)=>{
+    req.io =io;
+    req.connectedUsers = connectedUsers;
+    
+    return next();
+})
+
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname,'..','uploads')));
@@ -25,4 +49,4 @@ app.use(routes)
 
 
 
-app.listen(3333);
+server.listen(3333);
